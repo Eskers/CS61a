93cd07815ecf7429bc368e9891ba0642f139db76
Eskers <77648853+Eskers@users.noreply.github.com>
2023-09-23 18:33:08 +0100

    Q6 completed

--- practice/functions_control.py
+++ practice/functions_control.py
@@ -10,4 +10,6 @@ def distance(x1, y1, x2, y2):
     >>> distance(1, 2, 3, 4)
     2.8284271247461903
     """
-    "*** YOUR CODE HERE ***"
+    x_diff = x2 - x1
+    y_diff = y2 - y1
+    return sqrt(x_diff ** 2 + y_diff ** 2)
