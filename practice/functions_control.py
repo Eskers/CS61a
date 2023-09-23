6ddcca6b4a05310171e822d155a6f681d614debb
Eskers <77648853+Eskers@users.noreply.github.com>
2023-09-23 17:30:47 +0100

    Added file for wk1 practice problems

---
+++ practice/functions_control.py
@@ -0,0 +1,13 @@
+from math import sqrt
+
+def distance(x1, y1, x2, y2):
+    """Calculates the Euclidian distance between two points (x1, y1) and (x2, y2)
+
+    >>> distance(1, 1, 1, 2)
+    1.0
+    >>> distance(1, 3, 1, 1)
+    2.0
+    >>> distance(1, 2, 3, 4)
+    2.8284271247461903
+    """
+    "*** YOUR CODE HERE ***"
