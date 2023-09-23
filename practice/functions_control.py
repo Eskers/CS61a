94482a9bba90003ac8d297b8c7b0b1fefab188ca
Eskers <77648853+Eskers@users.noreply.github.com>
2023-09-23 18:44:34 +0100

    Completed Q7

--- practice/functions_control.py
+++ practice/functions_control.py
@@ -13,4 +13,19 @@ def distance(x1, y1, x2, y2):
     """
     x_diff = x2 - x1
     y_diff = y2 - y1
-    return sqrt(x_diff ** 2 + y_diff ** 2)
+    return sqrt(x_diff**2 + y_diff**2)
+
+
+def distance3d(x1, y1, z1, x2, y2, z2):
+    """Calculates the 3D Euclidian distance between two points (x1, y1, z1) and
+    (x2, y2, z2).
+
+    >>> distance3d(1, 1, 1, 1, 2, 1)
+    1.0
+    >>> distance3d(2, 3, 5, 5, 8, 3)
+    6.164414002968976
+    """
+    x_diff = x2 - x1
+    y_diff = y2 - y1
+    z_diff = z2 - z1
+    return sqrt(x_diff**2 + y_diff**2 + z_diff**2)
